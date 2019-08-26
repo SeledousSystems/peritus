88c157a2800ddc868d0e7cd43b83cbea420a9a4f
SeledousSystems <applevillas@gmail.com>
2019-08-26 16:46:55 +1200

    DB and Scheme development

---
+++ models/CPDObject.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var CPDPObjectSchema = new Schema({
+
+    title: {
+        type: String,
+        required: true,
+        max: 100
+    },
+    date_started: {
+        type: Date
+    },
+    date_completed: {
+        type: Date
+    },
+    status: {
+        type: String,
+        enum: ['Planning', 'In-Work', 'completed', 'On-Hold']
+    },
+    date_attended: {
+        type: Date
+    },
+    CPD_type: {
+        type: String,
+        enum: ['Training', 'Work', 'Accademic', 'Volunteering', 'Events', 'Self-Study']
+    },
+    reference: {
+        type: String
+    },
+    CPD_hours: {
+        type: Number
+    }
+})
+
+
+
+//Export model
+module.exports = mongoose.model('CPDObject', CPDObjectSchema);
